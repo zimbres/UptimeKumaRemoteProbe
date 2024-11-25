d7b3f6493ff08f6e7a28d828f7dbc952b56d7b53
OUMLALA Omar <omar.oumlala@orange.com>
2024-11-25 18:49:58 +0100

    feat: Add AllResults option for HTTP monitoring with status reporting

--- UptimeKumaRemoteProbe/Worker.cs
+++ UptimeKumaRemoteProbe/Worker.cs
@@ -110,7 +110,7 @@ public class Worker : BackgroundService
                     Port = int.Parse(monitor.Tags.Where(w => w.Name == "Port").Select(s => s.Value).FirstOrDefault() ?? "0"),
                     Domain = monitor.Tags.Where(w => w.Name == "Domain").Select(s => s.Value).FirstOrDefault() ?? string.Empty,
                     CertificateExpiration = int.Parse(monitor.Tags.Where(w => w.Name == "CertificateExpiration").Select(s => s.Value).FirstOrDefault() ?? "3"),
-                    IgnoreSSL = bool.Parse(monitor.Tags.Where(w => w.Name == "IgnoreSSL").Select(s => s.Value).FirstOrDefault() ?? "False")
+                    IgnoreSSL = bool.Parse(monitor.Tags.Where(w => w.Name == "IgnoreSSL").Select(s => s.Value).FirstOrDefault() ?? "False"),
                     AllResults = = bool.Parse(monitor.Tags.Where(w => w.Name == "AllResults").Select(s => s.Value).FirstOrDefault() ?? "False")
                 };
                 endpoints.Add(endpoint);
